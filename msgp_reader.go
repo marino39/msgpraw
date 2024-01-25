f82f662f57451cc9ac031a8b7fa8aa863ad081e8
Marcin Gorzynski <marcin@gorzynski.me>
2024-01-25 16:57:53 +0100

    Allow change index

--- msgp_reader.go
+++ msgp_reader.go
@@ -15,16 +15,16 @@ type IMsgpReader interface {
 
 type MsgpReader struct {
 	Buff []byte
-	idx  int
+	Idx  int
 }
 
 func (r *MsgpReader) Read() (Type, int, []byte, error) {
-	if r.idx >= len(r.Buff) {
+	if r.Idx >= len(r.Buff) {
 		return Type(0), 0, nil, EOF
 	}
 
-	msgpType := Type(r.Buff[r.idx])
-	r.idx++
+	msgpType := Type(r.Buff[r.Idx])
+	r.Idx++
 
 	if msgpType == Nil {
 		return msgpType, 0, nil, nil
@@ -37,66 +37,66 @@ func (r *MsgpReader) Read() (Type, int, []byte, error) {
 	} else if msgpType >= NegFixInt {
 		return msgpType, 0, nil, nil
 	} else if msgpType == Int8 || msgpType == Uint8 {
-		r.idx++
-		return msgpType, 0, r.Buff[r.idx-1 : r.idx], nil
+		r.Idx++
+		return msgpType, 0, r.Buff[r.Idx-1 : r.Idx], nil
 	} else if msgpType == Int16 || msgpType == Uint16 {
-		r.idx += 2
-		return msgpType, 0, r.Buff[r.idx-2 : r.idx], nil
+		r.Idx += 2
+		return msgpType, 0, r.Buff[r.Idx-2 : r.Idx], nil
 	} else if msgpType == Int32 || msgpType == Uint32 || msgpType == Float32 {
-		r.idx += 4
-		return msgpType, 0, r.Buff[r.idx-4 : r.idx], nil
+		r.Idx += 4
+		return msgpType, 0, r.Buff[r.Idx-4 : r.Idx], nil
 	} else if msgpType == Int64 || msgpType == Uint64 || msgpType == Float64 {
-		r.idx += 8
-		return msgpType, 0, r.Buff[r.idx-8 : r.idx], nil
+		r.Idx += 8
+		return msgpType, 0, r.Buff[r.Idx-8 : r.Idx], nil
 	} else if msgpType == Bin8 || msgpType == Ext8 || msgpType == Str8 {
-		length := int(r.Buff[r.idx])
-		r.idx++
-		return msgpType, 0, r.Buff[r.idx : r.idx+length], nil
+		length := int(r.Buff[r.Idx])
+		r.Idx++
+		return msgpType, 0, r.Buff[r.Idx : r.Idx+length], nil
 	} else if msgpType == Bin16 || msgpType == Ext16 || msgpType == Str16 {
-		length := int(r.Buff[r.idx])<<8 | int(r.Buff[r.idx+1])
-		r.idx += 2
-		return msgpType, 0, r.Buff[r.idx : r.idx+length], nil
+		length := int(r.Buff[r.Idx])<<8 | int(r.Buff[r.Idx+1])
+		r.Idx += 2
+		return msgpType, 0, r.Buff[r.Idx : r.Idx+length], nil
 	} else if msgpType == Bin32 || msgpType == Ext32 || msgpType == Str32 {
-		length := int(r.Buff[r.idx])<<24 | int(r.Buff[r.idx+1])<<16 | int(r.Buff[r.idx+2])<<8 | int(r.Buff[r.idx+3])
-		r.idx += 4
-		return msgpType, 0, r.Buff[r.idx : r.idx+length], nil
+		length := int(r.Buff[r.Idx])<<24 | int(r.Buff[r.Idx+1])<<16 | int(r.Buff[r.Idx+2])<<8 | int(r.Buff[r.Idx+3])
+		r.Idx += 4
+		return msgpType, 0, r.Buff[r.Idx : r.Idx+length], nil
 	} else if msgpType == FixExt1 {
-		r.idx++
-		return msgpType, 0, r.Buff[r.idx : r.idx+1], nil
+		r.Idx++
+		return msgpType, 0, r.Buff[r.Idx : r.Idx+1], nil
 	} else if msgpType == FixExt2 {
-		r.idx += 2
-		return msgpType, 0, r.Buff[r.idx-2 : r.idx], nil
+		r.Idx += 2
+		return msgpType, 0, r.Buff[r.Idx-2 : r.Idx], nil
 	} else if msgpType == FixExt4 {
-		r.idx += 4
-		return msgpType, 0, r.Buff[r.idx-4 : r.idx], nil
+		r.Idx += 4
+		return msgpType, 0, r.Buff[r.Idx-4 : r.Idx], nil
 	} else if msgpType == FixExt8 {
-		r.idx += 8
-		return msgpType, 0, r.Buff[r.idx-8 : r.idx], nil
+		r.Idx += 8
+		return msgpType, 0, r.Buff[r.Idx-8 : r.Idx], nil
 	} else if msgpType == FixExt16 {
-		r.idx += 16
-		return msgpType, 0, r.Buff[r.idx-16 : r.idx], nil
+		r.Idx += 16
+		return msgpType, 0, r.Buff[r.Idx-16 : r.Idx], nil
 	} else if msgpType >= FixArray && msgpType <= FixArrayMax {
 		length := int(msgpType) - int(FixArray)
-		return msgpType, length, r.Buff[r.idx:], nil
+		return msgpType, length, r.Buff[r.Idx:], nil
 	} else if msgpType == Array16 {
-		length := int(r.Buff[r.idx])<<8 | int(r.Buff[r.idx+1])
-		r.idx += 2
-		return msgpType, length, r.Buff[r.idx:], nil
+		length := int(r.Buff[r.Idx])<<8 | int(r.Buff[r.Idx+1])
+		r.Idx += 2
+		return msgpType, length, r.Buff[r.Idx:], nil
 	} else if msgpType == Array32 {
-		length := int(r.Buff[r.idx])<<24 | int(r.Buff[r.idx+1])<<16 | int(r.Buff[r.idx+2])<<8 | int(r.Buff[r.idx+3])
-		r.idx += 4
-		return msgpType, length, r.Buff[r.idx:], nil
+		length := int(r.Buff[r.Idx])<<24 | int(r.Buff[r.Idx+1])<<16 | int(r.Buff[r.Idx+2])<<8 | int(r.Buff[r.Idx+3])
+		r.Idx += 4
+		return msgpType, length, r.Buff[r.Idx:], nil
 	} else if msgpType >= FixMap && msgpType <= FixMapMax {
 		length := int(msgpType) - int(FixMap)
-		return msgpType, length, r.Buff[r.idx:], nil
+		return msgpType, length, r.Buff[r.Idx:], nil
 	} else if msgpType == Map16 {
-		length := int(r.Buff[r.idx])<<8 | int(r.Buff[r.idx+1])
-		r.idx += 2
-		return msgpType, length, r.Buff[r.idx:], nil
+		length := int(r.Buff[r.Idx])<<8 | int(r.Buff[r.Idx+1])
+		r.Idx += 2
+		return msgpType, length, r.Buff[r.Idx:], nil
 	} else if msgpType == Map32 {
-		length := int(r.Buff[r.idx])<<24 | int(r.Buff[r.idx+1])<<16 | int(r.Buff[r.idx+2])<<8 | int(r.Buff[r.idx+3])
-		r.idx += 4
-		return msgpType, length, r.Buff[r.idx:], nil
+		length := int(r.Buff[r.Idx])<<24 | int(r.Buff[r.Idx+1])<<16 | int(r.Buff[r.Idx+2])<<8 | int(r.Buff[r.Idx+3])
+		r.Idx += 4
+		return msgpType, length, r.Buff[r.Idx:], nil
 	} else {
 		return msgpType, 0, nil, fmt.Errorf("unknown msgp type: %x", msgpType)
 	}
